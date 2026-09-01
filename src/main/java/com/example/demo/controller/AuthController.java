7b2698cedbeab749d8cbf827dc38dfbf0b8104fb
Nadesan <Silambarasan.Nadesan@agilysys.com>
2026-09-01 10:51:53 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "Hello Spring Boot!!!";
+    }
+}
